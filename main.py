e54f67c500a63d16adb104ec649f21161f4363be
Soebb <87156166+Soebb@users.noreply.github.com>
2021-07-15 17:11:01 +0430

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import os
 import re
 import time
 import urllib.parse
-from functools import wraps
 from pathlib import Path
 from uuid import uuid4
 
 import telegram
 from logzero import logger
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters)
 from tinydb import TinyDB, Query
 
 from markdown_handler import MarkdownConverter
@@ -93,71 +92,6 @@ def done(update, context):
         context.user_data.clear()
 
 
-def post(update, context):
-    user_id = update.effective_user.id
-    context.bot.send_chat_action(chat_id=user_id, action=telegram.ChatAction.TYPING)
-    query = update.callback_query
-    query_data = query.data.split(';')
-    message_id = query_data[0]
-    search = db.get(user_db['message_id'] == message_id)
-    json_str = json.dumps(search)
-    resp = json.loads(json_str)
-    data = resp.get('text')
-    try:
-        if query_data[1] == "show_dialogs":
-            text = "\n".join(data)
-            url_msg = MarkdownConverter().convert(text)
-            share_url = 'tg://msg_url?url=' + urllib.parse.quote(url_msg)
-            markup = InlineKeyboardMarkup([[InlineKeyboardButton("📬 Share", url=share_url)], [
-                InlineKeyboardButton("📢 Publish to channel", callback_data='{}'.format(message_id)),
-                InlineKeyboardButton("🙈 Hide names", callback_data='{};hide_dialogs'.format(message_id))]])
-            query.edit_message_text(text=text, reply_markup=markup, parse_mode=ParseMode.HTML)
-        elif query_data[1] == "hide_dialogs":
-            text = "\n".join([i.split(': ', 1)[1] for i in data])
-            url_msg = MarkdownConverter().convert(text)
-            share_url = 'tg://msg_url?url=' + urllib.parse.quote(url_msg)
-            markup = InlineKeyboardMarkup([[InlineKeyboardButton("📬 Share", url=share_url)], [
-                InlineKeyboardButton("📢 Publish to channel", callback_data='{}'.format(message_id)),
-                InlineKeyboardButton("🗣 Show names", callback_data='{};show_dialogs'.format(message_id))]])
-            query.edit_message_text(text=text, reply_markup=markup, parse_mode=ParseMode.HTML)
-        else:
-            search = db.search(user_db['user_id'] == str(user_id))
-            json_str = json.dumps(search[0])
-            resp = json.loads(json_str)
-            channel_id = resp['channel_id']
-            text = "\n".join([i.split(': ', 1)[1] for i in data])
-            text = MarkdownConverter().convert(text)
-            context.bot.send_message(chat_id=channel_id, text=text, parse_mode=ParseMode.HTML)
-            context.bot.answer_callback_query(query.id, text="The message has been posted on your channel.",
-                                              show_alert=False)
-    except TypeError:
-        context.bot.send_message(chat_id=user_id,
-                                 text="I am unable to retrieve and process this message, please forward this "
-                                      "again.")
-    except IndexError:
-        context.bot.send_message(chat_id=user_id, text="You haven't added any channel yet, send /add followed by your "
-                                                       "channel's id which can be found using @ChannelIdBot.")
-
-
-def add(update, context):
-    user_id = update.message.from_user.id
-    channel_id = ' '.join(context.args)
-    context.bot.send_message(chat_id=channel_id, text="Your channel has been successfully added!")
-    context.bot.send_message(chat_id=user_id, text="Your channel has been successfully added!")
-
-
-def backup(update, context):
-    update.message.reply_document(document=open('db.json', 'rb'))
-
-
-def backup_handler(update, context):
-    file = context.bot.get_file(update.message.document.file_id)
-    file_name = update.message.document.file_name
-    os.remove(file_name)
-    file.download(file_name)
-    update.message.reply_text(text="Alright! I have uploaded the backup.")
-
-
 def error_callback(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
@@ -168,13 +102,9 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", send_help))
-    dp.add_handler(CommandHandler("add", add))
-    dp.add_handler((CallbackQueryHandler(post)))
     dp.add_handler(CommandHandler("done", done))
     dp.add_handler(CommandHandler("split", split_messages))
     dp.add_handler(MessageHandler(Filters.forwarded & Filters.text, store_forwarded_message))
-    dp.add_handler(CommandHandler("backup", backup))
-    dp.add_handler(MessageHandler(Filters.document, backup_handler))
     dp.add_error_handler(error_callback)
     updater.start_polling()
     logger.info("Ready to rock..!")
